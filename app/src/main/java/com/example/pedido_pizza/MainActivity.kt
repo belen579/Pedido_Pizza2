329366f62fc6ffc2e0ab8291de6abbe0c42ec75f
belen579 <belenbastosdavila@hotmail.com>
2023-11-13 21:19:28 +0100

    Realizado binding y AlertDialog

--- app/src/main/java/com/example/pedido_pizza/MainActivity.kt
+++ app/src/main/java/com/example/pedido_pizza/MainActivity.kt
@@ -10,40 +10,27 @@ import android.widget.CheckBox
 import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+import com.example.pedido_pizza.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-
-        var botonvalidar: Button = findViewById(R.id.boton)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val selectedIngredients = mutableListOf<String>()
         var ticketingredientes = ""
-
-        var champiñones: CheckBox = findViewById(R.id.champiñones)
-        var atun: CheckBox = findViewById(R.id.atun)
-        var aceitunas: CheckBox = findViewById(R.id.aceitunas)
-        var piña: CheckBox = findViewById(R.id.piña)
-        var extra: CheckBox = findViewById(R.id.extra)
-        var pepino: CheckBox = findViewById(R.id.checkBox)
-        var ticket: TextView = findViewById(R.id.ticket)
-        var recogidatienda: RadioButton = findViewById(R.id.recogida)
-        var reparto: RadioButton = findViewById(R.id.reparto)
-        var limpiar: Button = findViewById(R.id.limpiar)
-
-        var cadena: String = "*"
         var toast: Toast? = null
 
+        binding.boton.setOnClickListener() {
 
 
-
-        botonvalidar.setOnClickListener() {
-
-
-            if (champiñones.isChecked) {
+            if (binding.champiOnes.isChecked) {
                 selectedIngredients.add(ingredientes.champiñones.toString())
 
 
@@ -51,35 +38,35 @@ class MainActivity : AppCompatActivity() {
                 selectedIngredients.remove(ingredientes.champiñones.toString())
             }
 
-            if (atun.isChecked) {
+            if (binding.atun.isChecked) {
                 selectedIngredients.add(ingredientes.atun.toString())
 
             } else {
                 selectedIngredients.remove(ingredientes.atun.toString())
             }
 
-            if (aceitunas.isChecked) {
+            if (binding.aceitunas.isChecked) {
                 selectedIngredients.add(ingredientes.aceitunas.toString())
 
 
             } else {
                 selectedIngredients.remove(ingredientes.aceitunas.toString())
             }
-            if (piña.isChecked) {
+            if (binding.piA.isChecked) {
                 selectedIngredients.add(ingredientes.piña.toString())
 
             } else {
                 selectedIngredients.remove(ingredientes.piña.toString())
             }
 
-            if (extra.isChecked) {
+            if (binding.extra.isChecked) {
                 selectedIngredients.add(ingredientes.mozarella.toString())
 
             } else {
                 selectedIngredients.remove(ingredientes.mozarella.toString())
             }
 
-            if (pepino.isChecked) {
+            if (binding.pepino.isChecked) {
                 selectedIngredients.add(ingredientes.pepino.toString())
 
             } else {
@@ -89,40 +76,40 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
             if (selectedIngredients.isNotEmpty()) {
                 ticketingredientes =
                     "Ticket de pedido:\nIngredientes: ${selectedIngredients.joinToString(", ")}"
-                ticket.text = ticketingredientes
+                binding.ticket.text = ticketingredientes
 
 
             } else {
-                ticket.text = "Ningún ingrediente seleccionado."
+                binding.ticket.text = "Ningún ingrediente seleccionado."
                // toast= Toast.makeText(this, "No ha marcado ningún ingrediente", Toast.LENGTH_SHORT)
             }
 
 
-            if (reparto.isChecked) {
+            if (binding.reparto.isChecked) {
 
 
-                ticket.text =
+                binding.ticket.text =
                          ticketingredientes + " \n Ha seleccionado: "+ " \n Reparto a domicilio"
-            } else if( !reparto.isChecked && !recogidatienda.isChecked) {
-                ticket.setTextColor(Color.WHITE)
+                showalert()
+            } else if( !binding.reparto.isChecked && !binding.recogida.isChecked) {
+                binding.ticket.setTextColor(Color.WHITE)
                 toast = Toast.makeText(this, "No ha marcado ningúna opción de recogida", Toast.LENGTH_SHORT)
                 toast?.show()
 
                 }
 
-            if(recogidatienda.isChecked){
-                ticket.setTextColor(Color.BLACK)
-                ticket.text=
+            if(binding.recogida.isChecked){
+                binding.ticket.setTextColor(Color.BLACK)
+                binding.ticket.text=
                         ticketingredientes + "\n Ha seleccionado:"+"\n Recogida en tienda"
 
+                showalert()
 
-
-                }else if( !reparto.isChecked && !recogidatienda.isChecked) {
-                ticket.setTextColor(Color.WHITE)
+                }else if( !binding.reparto.isChecked && !binding.recogida.isChecked) {
+                binding.ticket.setTextColor(Color.WHITE)
                     toast =  Toast.makeText(this, "No ha marcado ningúna opción de recogida", Toast.LENGTH_SHORT)
 
                 toast?.show()
@@ -134,27 +121,24 @@ class MainActivity : AppCompatActivity() {
 
 
 
-            limpiar.setOnClickListener() {
-                champiñones.isChecked = false
-                atun.isChecked = false
-                aceitunas.isChecked = false
-                extra.isChecked = false
-                pepino.isChecked = false
-                piña.isChecked = false
-                reparto.isChecked= false
-                recogidatienda.isChecked= false
+            binding.limpiar.setOnClickListener() {
+                binding.champiOnes.isChecked = false
+                binding.atun.isChecked = false
+                binding.aceitunas.isChecked = false
+                binding.extra.isChecked = false
+                binding.pepino.isChecked = false
+                binding.piA.isChecked = false
+                binding.reparto.isChecked= false
+                binding.recogida.isChecked= false
 
-                ticket.text = ""
+                binding.ticket.text = ""
                 selectedIngredients.clear()
                 toast?.cancel()
 
             }
 
 
-            val ticketactivity2 =  ticket.text;
-            val intent = Intent(this, MainActivity3::class.java);
-            intent.putExtra("Ticket", ticketactivity2)
-            startActivity(intent)
+
 
 
 
@@ -174,6 +158,48 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+
+         fun showalert(){
+
+
+             val alert = AlertDialog.Builder(this)
+
+             alert.setTitle("Confirmación Pedido")
+             alert.setMessage("¿Desea validar el pedido?")
+
+
+             alert.setPositiveButton("Si"){
+                     dialog, which->
+                 var toast = Toast.makeText(this, "Pedido validado", Toast.LENGTH_SHORT)
+                 toast.show()
+
+                 val ticketactivity2 =  binding.ticket.text;
+
+                 val intent = Intent(this, MainActivity3::class.java);
+                 intent.putExtra("Ticket", ticketactivity2)
+                 startActivity(intent)
+
+
+
+                     dialog.dismiss()
+                 }
+
+               alert.setNegativeButton("No"){
+                   dialog, which->
+                   var toast = Toast.makeText(this, "Pedido cancelado", Toast.LENGTH_SHORT)
+                   toast.show()
+                   dialog.dismiss()
+               }
+
+
+
+
+
+             alert.create()
+             alert.show()
+
+
+         }
     }
 
 
--- app/src/main/java/com/example/pedido_pizza/MainActivityportada.kt
+++ app/src/main/java/com/example/pedido_pizza/MainActivityportada.kt
@@ -7,10 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivityportada : AppCompatActivity() {
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_activityportada)
-
 
 
 
